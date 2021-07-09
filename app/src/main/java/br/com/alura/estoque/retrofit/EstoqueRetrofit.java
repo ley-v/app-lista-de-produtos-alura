2c1f564eb1cc575a682665413063be24a4771eac
ley-v <83968941+ley-v@users.noreply.github.com>
2021-07-09 13:38:16 +0100

    Utilizando a biblioteca Retrofit, criando a instância, service e conversor

---
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -0,0 +1,23 @@
+package br.com.alura.estoque.retrofit;
+
+import br.com.alura.estoque.retrofit.service.ProdutoService;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class EstoqueRetrofit {
+
+    private final ProdutoService produtoService;
+
+    public EstoqueRetrofit(){
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.56.1:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        produtoService = retrofit.create(ProdutoService.class);
+    }
+
+    public ProdutoService getProdutoService() {
+        return produtoService;
+    }
+}
+
