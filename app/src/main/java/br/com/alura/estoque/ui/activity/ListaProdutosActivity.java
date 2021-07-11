fa1bc147a01e1036a122c7e18c7e90892fdc456d
ley-v <83968941+ley-v@users.noreply.github.com>
2021-07-11 13:55:44 +0100

    Criando a classe Repositório para integrar comportamentos entre banco de dados interno e externo, e implementando comportamento de busca integrado

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -1,14 +1,12 @@
 package br.com.alura.estoque.ui.activity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.IOException;
 import java.util.List;
 
 import br.com.alura.estoque.R;
@@ -16,13 +14,10 @@ import br.com.alura.estoque.asynctask.BaseAsyncTask;
 import br.com.alura.estoque.database.EstoqueDatabase;
 import br.com.alura.estoque.database.dao.ProdutoDAO;
 import br.com.alura.estoque.model.Produto;
-import br.com.alura.estoque.retrofit.EstoqueRetrofit;
-import br.com.alura.estoque.retrofit.service.ProdutoService;
+import br.com.alura.estoque.repository.ProdutoRepository;
 import br.com.alura.estoque.ui.dialog.EditaProdutoDialog;
 import br.com.alura.estoque.ui.dialog.SalvaProdutoDialog;
 import br.com.alura.estoque.ui.recyclerview.adapter.ListaProdutosAdapter;
-import retrofit2.Call;
-import retrofit2.Response;
 
 public class ListaProdutosActivity extends AppCompatActivity {
 
@@ -42,30 +37,11 @@ public class ListaProdutosActivity extends AppCompatActivity {
         EstoqueDatabase db = EstoqueDatabase.getInstance(this);
         dao = db.getProdutoDAO();
 
-        buscaProdutos();
+        ProdutoRepository repository = new ProdutoRepository(dao);
+        repository.buscaProdutos(adapter::atualiza);
     }
 
-    private void buscaProdutos() {
-//        new BaseAsyncTask<>(dao::buscaTodos,
-//                resultado -> adapter.atualiza(resultado))
-//                .execute();
-        ProdutoService service = new EstoqueRetrofit().getProdutoService();
-        Call<List<Produto>> call = service.buscaTodos();
-        new BaseAsyncTask<>(() ->
-        {
-            try {
-                Response<List<Produto>> resposta = call.execute();
-                List<Produto> produtosNovos = resposta.body();
-                return produtosNovos;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }, produtosNovos -> {
-if(produtosNovos != null) adapter.atualiza(produtosNovos);
-else Toast.makeText(this, "Falha na busca de produtos -web", Toast.LENGTH_LONG).show();
-        }).execute();
-    }
+
 
     private void configuraListaProdutos() {
         RecyclerView listaProdutos = findViewById(R.id.activity_lista_produtos_lista);
