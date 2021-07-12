3aed12975b45fa3681dd8f18c3c80358875a060c
ley-v <83968941+ley-v@users.noreply.github.com>
2021-07-12 15:30:15 +0100

    Integrando comportamentos de Inserção, implementando o método enqueue e utilizando callbacks para tratar resposta de requisições web

--- app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
+++ app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.estoque.repository;
 
 import android.os.AsyncTask;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -11,21 +13,25 @@ import br.com.alura.estoque.model.Produto;
 import br.com.alura.estoque.retrofit.EstoqueRetrofit;
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.internal.EverythingIsNonNull;
 
 public class ProdutoRepository {
 
     private final ProdutoDAO dao;
+    private ProdutoService service;
 
     public ProdutoRepository(ProdutoDAO dao) {
         this.dao = dao;
+        service = new EstoqueRetrofit().getProdutoService();
     }
 
-    public void buscaProdutos(ProdutosCarregadosListener listener) {
+    public void buscaProdutos(DadosCarregadosListener<List<Produto>> listener) {
         buscaProdutosInternos(listener);
     }
 
-    private void buscaProdutosInternos(ProdutosCarregadosListener listener) {
+    private void buscaProdutosInternos(DadosCarregadosListener<List<Produto>> listener) {
         new BaseAsyncTask<>(dao::buscaTodos,
                 resultado -> {
                     listener.quandoCarregados(resultado);
@@ -33,8 +39,7 @@ public class ProdutoRepository {
                 }).execute();
     }
 
-    private void buscaProdutosNaApi(ProdutosCarregadosListener listener) {
-        ProdutoService service = new EstoqueRetrofit().getProdutoService();
+    private void buscaProdutosNaApi(DadosCarregadosListener<List<Produto>> listener) {
         Call<List<Produto>> call = service.buscaTodos();
         new BaseAsyncTask<>(() ->
         {
@@ -51,7 +56,48 @@ public class ProdutoRepository {
         }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
-    public interface ProdutosCarregadosListener{
-        void quandoCarregados(List<Produto> produtos);
+    public void salva(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        salvaNaApi(produto, callback);
+    }
+
+    private void salvaNaApi(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        Call<Produto> call = service.salva(produto);
+        call.enqueue(new Callback<Produto>() {
+            @Override
+            @EverythingIsNonNull
+            public void onResponse(Call<Produto> call, Response<Produto> response) {
+                if (response.isSuccessful()) {
+                    Produto produtoSalvo = response.body();
+                    if (produtoSalvo != null) salvaInterno(produtoSalvo, callback);
+                }
+                else {
+                    callback.quandoFalha("Resposta mal sucedida");
+                }
+            }
+
+            @Override
+            @EverythingIsNonNull
+            public void onFailure(Call<Produto> call, Throwable t) {
+                callback.quandoFalha("Falha de comunicação: "+ t.getMessage());
+            }
+        });
+    }
+
+    private void salvaInterno(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        new BaseAsyncTask<>(() -> {
+            long id = dao.salva(produto);
+            return dao.buscaProduto(id);
+        }, resultado -> callback.quandoSucesso(resultado)
+        )
+                .execute();
+    }
+
+    public interface DadosCarregadosListener<T> {
+        void quandoCarregados(T resultado);
+    }
+
+    public interface DadosCarregadosCallback<T>{
+        void quandoSucesso(T resultado);
+        void quandoFalha(String erro);
     }
 }
